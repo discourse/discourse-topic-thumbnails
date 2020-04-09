34b60e510cbfa213b321a5eb8d660d23985195b5
David Taylor <david@taylorhq.com>
2020-04-09 15:11:14 +0100

    Remove unused code

--- javascripts/discourse/initializers/topic-thumbnails-init.js
+++ javascripts/discourse/initializers/topic-thumbnails-init.js
@@ -11,26 +11,7 @@ export default {
 
   initWithApi(api){
     const site = api.container.lookup("site:main");
-    // api.modifyClass("component:topic-list-item", {
-    //   templateName(){
-    //     if(settings.show_thumbnails){
-    //       return "list/thumbnail-topic-list-item"
-    //     }else{
-    //       return "list/topic-list-item";
-    //     }
-    //   },
-
-    //   // Override the core method, so that we can change the template dynamically
-    //   @observes("topic.pinned")
-    //   renderTopicListItem() {
-    //     const template = findRawTemplate(this.templateName());
-    //     if (template) {
-    //       this.set("topicListItemContents", template(this).htmlSafe());
-    //     }
-    //   },
-    // });
-
-    console.log("mobile", site.mobileView)
+    
     api.modifyClass("component:topic-list-item", {
       expandPinned: site.mobileView ? settings.show_excerpts_mobile : settings.show_excerpts_desktop
     })
