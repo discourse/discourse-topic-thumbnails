c6fed0b422924c622d99f15c43a5216cf6528293
David Taylor <david@taylorhq.com>
2022-09-21 12:35:21 +0100

    FEATURE: Add new setting to control suggested topics display mode (#21)

---
+++ test/acceptance/suggested-topics-test.js
@@ -0,0 +1,36 @@
+import {
+  acceptance,
+  exists,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
+import { visit } from "@ember/test-helpers";
+import { test } from "qunit";
+
+acceptance("topic list thumbnails | suggested topics", function () {
+  test("list the tags", async function (assert) {
+    await visit("/t/280");
+    assert.true(
+      exists("#suggested-topics .topic-list"),
+      "suggested topics is displayed"
+    );
+    const defaultClassNames = Array.from(
+      query("#suggested-topics .topic-list").classList
+    );
+    assert.false(
+      defaultClassNames.some((c) => c.startsWith("topic-thumbnails-")),
+      "uses the default view"
+    );
+
+    settings.suggested_topics_mode = "masonry";
+
+    await visit("/t/280");
+    assert.true(
+      exists("#suggested-topics .topic-list"),
+      "suggested topics is displayed"
+    );
+    const newClassNames = Array.from(
+      query("#suggested-topics .topic-list").classList
+    );
+    assert.true(newClassNames.includes("topic-thumbnails-masonry"));
+  });
+});
