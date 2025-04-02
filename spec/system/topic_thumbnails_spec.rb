92bb96a501ca6076018c7d3891cb59dee843ca44
David Taylor <david@taylorhq.com>
2025-04-02 11:59:17 +0100

    DEV: Drop raw-hbs specs
    In preparation for https://github.com/discourse/discourse/pull/32081

--- spec/system/topic_thumbnails_spec.rb
+++ spec/system/topic_thumbnails_spec.rb
@@ -7,56 +7,32 @@ RSpec.describe "Topic Thumbnails", type: :system do
 
   before { sign_in user }
 
-  RSpec.shared_examples "topic thumbnails" do
-    {
-      "grid" => "grid",
-      "minimal-grid" => "minimal",
-      "list" => "list",
-      "blog-style" => "blog-style-grid",
-    }.each do |style, class_name|
-      it "renders topic thumbnails in #{style} style" do
-        theme.update_setting(:default_thumbnail_mode, style)
-        theme.save!
-
-        visit "/latest"
-
-        expect(page).to have_css(".topic-list.topic-thumbnails-#{class_name}")
-        expect(page).to have_css(".topic-list-thumbnail", count: 5)
-      end
-    end
-
-    it "renders topic thumbnails in masonry style" do
-      theme.update_setting(:default_thumbnail_mode, "masonry")
+  {
+    "grid" => "grid",
+    "minimal-grid" => "minimal",
+    "list" => "list",
+    "blog-style" => "blog-style-grid",
+  }.each do |style, class_name|
+    it "renders topic thumbnails in #{style} style" do
+      theme.update_setting(:default_thumbnail_mode, style)
       theme.save!
 
       visit "/latest"
 
-      expect(page).to have_css(".topic-list.topic-thumbnails-masonry")
+      expect(page).to have_css(".topic-list.topic-thumbnails-#{class_name}")
       expect(page).to have_css(".topic-list-thumbnail", count: 5)
-
-      expect(page).to have_css(".topic-list-item.masonry-0")
     end
   end
 
-  context "with default raw-hbs topic list" do
-    before { SiteSetting.glimmer_topic_list_mode = "disabled" }
+  it "renders topic thumbnails in masonry style" do
+    theme.update_setting(:default_thumbnail_mode, "masonry")
+    theme.save!
 
-    it "is using legacy topic list" do
-      visit "/latest"
-      expect(page).to have_css(".topic-list.ember-view")
-    end
+    visit "/latest"
 
-    it_behaves_like "topic thumbnails"
-  end
+    expect(page).to have_css(".topic-list.topic-thumbnails-masonry")
+    expect(page).to have_css(".topic-list-thumbnail", count: 5)
 
-  context "with glimmer topic list" do
-    before { SiteSetting.glimmer_topic_list_mode = "auto" }
-
-    it "is using glimmer topic list" do
-      visit "/latest"
-      expect(page).to have_css(".topic-list:not(.ember-view)")
-    end
-
-    it_behaves_like "topic thumbnails"
+    expect(page).to have_css(".topic-list-item.masonry-0")
   end
 end
