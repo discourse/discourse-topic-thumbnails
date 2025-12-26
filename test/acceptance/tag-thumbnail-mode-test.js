d7551df35e302fdff3095a6e84aa41878e38082c
Nat <natalie.tay@discourse.org>
2025-12-26 20:47:51 +0800

    add test that will pass after merge

---
+++ test/acceptance/tag-thumbnail-mode-test.js
@@ -0,0 +1,32 @@
+import { getOwner } from "@ember/owner";
+import { visit } from "@ember/test-helpers";
+import { test } from "qunit";
+import { cloneJSON } from "discourse/lib/object";
+import discoveryFixture from "discourse/tests/fixtures/discovery-fixtures";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+
+acceptance("topic thumbnails | tag route integration", function (needs) {
+  needs.settings({ tagging_enabled: true });
+
+  needs.pretender((server, helper) => {
+    server.get("/tag/important/l/latest.json", () => {
+      return helper.response(
+        cloneJSON(discoveryFixture["/tag/important/l/latest.json"])
+      );
+    });
+  });
+
+  test("service correctly identifies tag name from route", async function (assert) {
+    await visit("/tag/important");
+
+    const topicThumbnailsService = getOwner(this).lookup(
+      "service:topic-thumbnails"
+    );
+
+    assert.strictEqual(
+      topicThumbnailsService.viewingTagName,
+      "important",
+      "viewingTagName returns the tag name (not id) from the route"
+    );
+  });
+});
